fd81ab61314c94a6b8abb42f07075b59c3f44d8c
Blake Wilson <blake@blake.id>
2024-04-26 19:40:27 -0500

    Create sharing button, seo info

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,17 @@
 "use client";
 
+import { ShareIcon } from "@heroicons/react/16/solid";
+import classNames from "classnames";
+import { useState } from "react";
+
 export default function Header() {
+  const shareLink = "https://tsconfig.guide";
+  let shareButtonText = "Share";
+  let shareButtonTextClicked = "Share link copied!";
+  const [isSharing, setIsSharing] = useState(false);
+
   return (
-    <header className="block lg:fixed z-40 bg-slate-900 w-full">
+    <header className={classNames("block lg:fixed z-40 bg-slate-900 w-full")}>
       <nav
         className="mx-auto flex max-w-full items-center justify-between p-6 lg:px-8"
         aria-label="Global"
@@ -15,9 +24,21 @@ export default function Header() {
         <div className="lg:flex lg:flex-1 lg:justify-end">
           <button
             type="button"
-            className="rounded-md bg-white/10 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-white/20"
+            onClick={() => {
+              navigator.clipboard.writeText(shareLink);
+              setIsSharing(true);
+
+              setTimeout(() => {
+                setIsSharing(false);
+              }, 2500);
+            }}
+            className={classNames(
+              "flex items-center rounded-md bg-white/10 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-white/20",
+              isSharing ? "bg-green-500 hover:bg-green-500" : undefined
+            )}
           >
-            Share
+            <ShareIcon className="mr-2" width={16} height={16} />{" "}
+            {isSharing ? shareButtonTextClicked : shareButtonText}
           </button>
         </div>
       </nav>
